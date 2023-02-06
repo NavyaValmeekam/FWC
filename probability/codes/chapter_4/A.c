a54b0f7f554906eb66780e4862a3374af20054a0
NavyaValmeekam <111700288+NavyaValmeekam@users.noreply.github.com>
2023-02-06 12:02:06 +0530

    Add files via upload

---
+++ probability/codes/chapter_4/A.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void uniform1(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+
+}
+void uniform2(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp=fopen(str,"w");
+for(i=0;i<len;i++)
+{
+	fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+}
+void uniform3(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+
+}
+void uniform4(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp=fopen(str,"w");
+for(i=0;i<len;i++)
+{
+	fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+}
+void normalRandom1(char *str,int len) {
+int i=0;
+FILE *fp;
+FILE *fp1;
+FILE *fp2;
+double v1,v2,norm1;
+fp=fopen("normal1.dat","w");
+fp1=fopen("uni1.dat","r");
+fp2=fopen("uni2.dat","r");
+while((fscanf(fp1,"%lf",&v1)!=EOF) && (fscanf(fp2,"%lf",&v2)!=EOF))
+{
+	i=i+1;
+	norm1= cos(2*3.14*v2)*sqrt(-2.*log(v1));
+	fprintf(fp,"%lf\n",norm1);
+}
+fclose(fp2);
+fclose(fp1);
+fclose(fp);
+}
+void normalRandom2(char *str,int len) {
+int i=0;
+FILE *fp;
+FILE *fp1;
+FILE *fp2;
+double v3,v4,norm2;
+fp=fopen("normal2.dat","w");
+fp1=fopen("uni3.dat","r");
+fp2=fopen("uni4.dat","r");
+while((fscanf(fp1,"%lf",&v3)!=EOF) && (fscanf(fp2,"%lf",&v4)!=EOF))
+{
+	i=i+1;
+	norm2= cos(2*3.14*v4)*sqrt(-2.*log(v3));
+	fprintf(fp,"%lf\n",norm2);
+}
+fclose(fp2);
+fclose(fp1);
+fclose(fp);
+}
+void V(char *str,int len){
+int i=0;
+FILE *fp;
+FILE *fp1;
+FILE *fp2;
+double x1,x2,V;
+fp=fopen("V.dat","w");
+fp1=fopen("normal1.dat","r");
+fp2=fopen("normal2.dat","r");
+while((fscanf(fp1,"%lf",&x1)!=EOF) && (fscanf(fp2,"%lf",&x2)!=EOF))
+{
+	i=i+1;
+	V= x1*x1+x2*x2;
+	fprintf(fp,"%lf\n",V);
+}
+fclose(fp2);
+fclose(fp1);
+fclose(fp);
+}
+void A(char *str,int len){
+int i=0;
+FILE *fp;
+FILE *fp1;
+double x,A;
+fp=fopen("A.dat","w");
+fp1=fopen("V.dat","r");
+while(fscanf(fp1,"%lf",&x)!=EOF)
+{
+	i=i+1;
+	A= sqrt(x);
+	fprintf(fp,"%lf\n",A);
+}
+fclose(fp1);
+fclose(fp);
+}
+int main(void)
+{
+ 	uniform1("uni1.dat",1000000);
+	uniform2("uni2.dat",1000000);
+	uniform3("uni3.dat",1000000);
+	uniform4("uni4.dat",1000000);
+	normalRandom1("normal1.dat",1000000);
+	normalRandom2("normal2.dat",1000000);
+	V("V.dat",1000000);
+	A("A.dat",1000000);
+        return 0;
+}
---
+++ probability/codes/chapter_4/A_pdf.py
@@ -0,0 +1,53 @@
+import numpy as np
+import mpmath as mp
+import scipy 
+import matplotlib.pyplot as plt
+
+#if using termux
+import subprocess
+import shlex
+#end if
+
+
+maxrange=100
+maxlim=10.0
+x = np.linspace(0,maxlim,maxrange)#points on the x axis
+simlen = int(1e6) #number of samples
+err = [] #declaring probability list
+pdf = [] #declaring pdf list
+h = 2*maxlim/(maxrange-1);
+#randvar = np.random.normal(0,1,simlen)
+#randvar = np.loadtxt('uni.dat',dtype='double')
+#randvar = np.loadtxt('tri.dat',dtype='double')
+#randvar = np.loadtxt('V.dat',dtype='double')
+randvar = np.loadtxt('A.dat',dtype='double')
+for i in range(0,maxrange):
+	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+	err_n = np.size(err_ind) #computing the probability
+	err.append(err_n/simlen) #storing the probability values in a list
+
+	
+for i in range(0,maxrange-1):
+	test = (err[i+1]-err[i])/(x[i+1]-x[i])
+	pdf.append(test) #storing the pdf values in a list
+
+
+	 	 	
+#tri_pdf11 = scipy.vectorize(tri_pdf1)
+
+plt.plot(x[0:(maxrange-1)].T,pdf,'--')
+#plt.plot(x,tri_pdf12(x,0,2,1),'*')#plotting the PDF
+plt.grid() #creating the grid
+plt.xlabel('$x_i$')
+plt.ylabel('$p_X(x_i)$')
+plt.legend(["Numerical"])
+
+#if using termux
+#plt.savefig('figs/cdf/uni_pdf.pdf')
+#plt.savefig('figs/cdf/uni_pdf.eps')
+#subprocess.run(shlex.split("termux-open figs/cdf/uni_pdf.pdf"))
+plt.savefig('tri_pdf1.pdf')
+#plt.savefig('figs/clt/gauss_pdf.eps')
+#subprocess.run(shlex.split("termux-open figs/clt/gauss_pdf.pdf"))
+#else
+plt.show() #opening the plot window
